7c624e74f384d4c4a0c33904e9d285b756ee0b25
joe <josulliv101@gmail.com>
2024-02-08 10:44:53 -0500

    search btn styling

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -124,23 +124,23 @@ export default function NavMenu({ children }: React.PropsWithChildren) {
           <NavigationMenuItem className="hidden lg:inline-flex">
             <NavigationMenuTrigger>about</NavigationMenuTrigger>
             <NavigationMenuContent>
-              <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+              <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[1fr_1fr]">
                 <li className="row-span-3">
                   <NavigationMenuLink asChild>
                     <Link
-                      className="flex items-center h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                      className="flex items-center h-full w-full select-none flex-col justify-between rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
                       href="/"
                     >
                       <img
-                        width="48"
-                        height="48"
+                        width="80"
+                        height="80"
                         alt="whatsawesome"
                         src={config.logoPath}
-                        className="grayscale"
+                        className="grayscale pt-4"
                       />
-                      <div className="mb-2 mt-4 text-lg font-medium">
+                      {/* <div className="hidden mb-2 mt-4 text-lg font-medium">
                         whats awesome
-                      </div>
+                      </div> */}
                       <p className="text-sm leading-tight text-muted-foreground">
                         Find out what is awesome about people and places. Vote
                         on what you think is awesome or add you own awesome
