30b84b4b38026d3a3be761df23f275400f17d76e
joe <josulliv101@gmail.com>
2024-03-21 09:40:56 -0400

    add branch

--- src/app/explore/[hub]/page.tsx
+++ src/app/explore/[hub]/page.tsx
@@ -3,9 +3,9 @@ import { PropsWithChildren } from "react";
 export default function Page({
   params: { hub },
   searchParams: { pt, st },
-}: PropsWithChildren<{
+}: {
   params: any;
   searchParams: any;
-}>) {
+}) {
   return <div>hello page</div>;
 }
