9fe14b163cf10897c6bfef6c1c5499d087ebd577
joe <josulliv101@gmail.com>
2024-03-21 16:16:58 -0400

    add data

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,5 @@
 import { PropsWithChildren } from "react";
 
-export default function Page({
-  params: { id },
-}: PropsWithChildren<{
-  params: any;
-}>) {
+export default function Page({ params: { id } }: { params: any }) {
   return <div>main root profile {id}</div>;
 }
