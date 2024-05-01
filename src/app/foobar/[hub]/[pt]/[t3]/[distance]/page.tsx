e82597c4fe28d1a59406294ba64e12dd7f11ee75
joe <josulliv101@gmail.com>
2024-05-01 08:24:50 -0400

    added test directory

--- src/app/foobar/[hub]/[pt]/[t3]/[distance]/page.tsx
+++ src/app/foobar/[hub]/[pt]/[t3]/[distance]/page.tsx
@@ -1,13 +1,18 @@
+import { searchTopAoeByCategory } from "@/lib/search";
+
 export function generateStaticParams() {
   return [];
 }
 
-export const runtime = "edge";
+export default async function Page({ params: { hub, pt, t3, distance } }: any) {
+  const topProfiles = await searchTopAoeByCategory(hub, [[t3, pt]]);
 
-export default function Page({ params: { hub, pt, t3, distance } }: any) {
   return (
-    <div className="p-12">
-      foobar: {hub} / {pt} / {t3} / {distance}
-    </div>
+    <>
+      <div className="p-12">
+        foobar: {hub} / {pt} / {t3} / {distance}
+      </div>
+      <div>{JSON.stringify(topProfiles)}</div>
+    </>
   );
 }
