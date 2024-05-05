f5cb3bbb698ad4478be093b98ae374a723b8fbe7
joe <josulliv101@gmail.com>
2024-05-05 15:03:34 -0400

    adjust like btn logic

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,14 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
     <div className="bg-black text-white">
       <footer className="border-t flex justify-center p-6">
-        © 2024 Blue Mushroom / Discover excellence.
+        © 2024 Blue Mushroom /{" "}
+        <Link href={`/foobar/boston/restaurant/burger/20`}>
+          Discover excellence
+        </Link>
+        .
       </footer>
     </div>
   );
