feeb6201a71938acc73ed13cf4ac7b67cd526026
joe <josulliv101@gmail.com>
2024-05-14 11:24:31 -0400

    adjust icons

--- src/app/(foobargroup)/foobar/[hub]/[pt]/[t3]/[distance]/[pg]/SearchLogoTabs.tsx
+++ src/app/(foobargroup)/foobar/[hub]/[pt]/[t3]/[distance]/[pg]/SearchLogoTabs.tsx
@@ -1,3 +1,5 @@
+import GridIcon from "@/app/icons/GridIcon";
+import RowIcon from "@/app/icons/RowIcon";
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { EllipsisIcon, GripHorizontalIcon } from "lucide-react";
 import { PropsWithChildren } from "react";
@@ -11,13 +13,13 @@ export default function SearchLogoTabs({
     <>
       <Tabs value={value} onValueChange={onChange}>
         <TabsList className="grid grid-cols-2">
-          <TabsTrigger value="grid">
+          <TabsTrigger value="grid" className="max-w-[36px]">
             <span className="sr-only">Complete</span>
-            <GripHorizontalIcon className="w-6 h-6" />
+            <GridIcon className="w-4 h-4 text-muted-foreground" />
           </TabsTrigger>
-          <TabsTrigger value="row">
+          <TabsTrigger value="row" className="max-w-[36px]">
             <span className="sr-only">Insert</span>
-            <EllipsisIcon className="w-6 h-6" />
+            <RowIcon className="w-3.5 h-3.5 text-muted-foreground min-h-[16px]" />
           </TabsTrigger>
         </TabsList>
       </Tabs>
---
+++ src/app/icons/GridIcon.tsx
@@ -0,0 +1,159 @@
+import { LucideProps, createLucideIcon } from "lucide-react";
+// import createLucideIcon from '../createLucideIcon.js';
+
+const GridIcon = createLucideIcon("GridIcon", [
+  [
+    "rect",
+    {
+      width: "4",
+      height: "4",
+      x: "1",
+      y: "5",
+      rx: "0",
+      key: "rect1",
+      fill: "currentColor",
+    },
+  ],
+  [
+    "rect",
+    {
+      width: "4",
+      height: "4",
+      x: "9",
+      y: "5",
+      rx: "0",
+      key: "rect2",
+      fill: "currentColor",
+    },
+  ],
+  [
+    "rect",
+    {
+      width: "4",
+      height: "4",
+      x: "17",
+      y: "5",
+      rx: "0",
+      key: "rect3",
+      fill: "currentColor",
+    },
+  ],
+
+  [
+    "rect",
+    {
+      width: "4",
+      height: "4",
+      x: "1",
+      y: "13",
+      rx: "0",
+      key: "rect4",
+      fill: "currentColor",
+    },
+  ],
+  [
+    "rect",
+    {
+      width: "4",
+      height: "4",
+      x: "9",
+      y: "13",
+      rx: "0",
+      key: "rect5",
+      fill: "currentColor",
+    },
+  ],
+  [
+    "rect",
+    {
+      width: "4",
+      height: "4",
+      x: "17",
+      y: "13",
+      rx: "0",
+      key: "rect6",
+      fill: "currentColor",
+    },
+  ],
+]);
+
+export { GridIcon as default };
+
+// export function RowIcon({ ...props }: LucideProps) {
+//   return (
+//     <svg
+//       xmlns="http://www.w3.org/2000/svg"
+//       width="24"
+//       height="24"
+//       viewBox="0 0 24 24"
+//       fill="none"
+//       stroke="currentColor"
+//       stroke-width="2"
+//       stroke-linecap="round"
+//       stroke-linejoin="round"
+//       className="lucide lucide-grip-horizontal"
+//       scale={"2"}
+//     >
+//       <rect
+//         width="4"
+//         height="4"
+//         x="1"
+//         y="5"
+//         rx="0"
+//         ry="0"
+//         fill="currentColor"
+//       />
+//       <rect
+//         width="4"
+//         height="4"
+//         x="9"
+//         y="5"
+//         rx="0"
+//         ry="0"
+//         fill="currentColor"
+//       />
+//       <rect
+//         width="4"
+//         height="4"
+//         x="17"
+//         y="5"
+//         rx="0"
+//         ry="0"
+//         fill="currentColor"
+//       />
+//       <rect
+//         width="4"
+//         height="4"
+//         x="1"
+//         y="13"
+//         rx="0"
+//         ry="0"
+//         fill="currentColor"
+//       />
+//       <rect
+//         width="4"
+//         height="4"
+//         x="9"
+//         y="13"
+//         rx="0"
+//         ry="0"
+//         fill="currentColor"
+//       />
+//       <rect
+//         width="4"
+//         height="4"
+//         x="17"
+//         y="13"
+//         rx="0"
+//         ry="0"
+//         fill="currentColor"
+//       />
+//       {/* <circle cx="12" cy="9" r="1" />
+//       <circle cx="19" cy="9" r="1" />
+//       <circle cx="5" cy="9" r="1" />
+//       <circle cx="12" cy="15" r="1" />
+//       <circle cx="19" cy="15" r="1" />
+//       <circle cx="5" cy="15" r="1" /> */}
+//     </svg>
+//   );
+// }
