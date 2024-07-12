c58b83de7a63e424089b5ded9418001e61b256a4
joe <josulliv101@gmail.com>
2024-07-12 16:52:58 -0400

    add wistia video

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,79 +23,7 @@ export default function Page() {
       <div className="bg-gray-300/80 bg-[url('/support-waves.svg')] ">
         <div className="container mx-auto max-w-[1080px] pb-4 pt-6 h-full">
           <div className="grid grid-cols-12 rounded-lg overflow-hidden">
-            <div className="col-span-12 md:col-span-12 rounded-lg overflow-hidden bg-gray-100 h-[320px] p-8 flex gap-8 items-start w-full">
-              <div className="min-w-[200px] ">
-                <script
-                  src="https://fast.wistia.com/embed/medias/2h3f4pt7xf.jsonp"
-                  async
-                ></script>
-                <script
-                  src="https://fast.wistia.com/assets/external/E-v1.js"
-                  async
-                ></script>
-                <div
-                  className="wistia_responsive_padding"
-                  style={{
-                    padding: "100.0% 0 0 0",
-                    position: "relative",
-                  }}
-
-                  // style="padding:100.0% 0 0 0;position:relative;"
-                >
-                  <div
-                    className="wistia_responsive_wrapper"
-                    style={{
-                      height: "100%",
-                      left: 0,
-                      top: 0,
-                      width: "100%",
-                      position: "absolute",
-                    }}
-                    // style="height:100%;left:0;position:absolute;top:0;width:100%;"
-                  >
-                    <div
-                      className="wistia_embed wistia_async_2h3f4pt7xf seo=false videoFoam=true"
-                      style={{
-                        height: "100%",
-                        width: "100%",
-                        position: "relative",
-                      }}
-                      // style="height:100%;position:relative;width:100%"
-                    >
-                      <div
-                        className="wistia_swatch"
-                        style={{
-                          height: "100%",
-                          left: 0,
-                          top: 0,
-                          opacity: 0.5,
-                          width: "100%",
-                          position: "absolute",
-                          overflow: "hidden",
-                        }}
-                        // style="height:100%;left:0;opacity:0;overflow:hidden;position:absolute;top:0;transition:opacity 200ms;width:100%;"
-                      >
-                        <img
-                          src="https://fast.wistia.com/embed/medias/2h3f4pt7xf/swatch"
-                          style={{
-                            filter: "blur(5px)",
-                            height: "100%",
-                            objectFit: "contain",
-                            width: "100%",
-                          }}
-                          // style="filter:blur(5px);height:100%;object-fit:contain;width:100%;"
-                          alt=""
-                          aria-hidden="true"
-                          // onLoad="this.parentNode.style.opacity=1;"
-                        />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <p className="text-xs text-muted-foreground pt-4">
-                  &quot;Intro from Carmen&quot; (total: 10s)
-                </p>
-              </div>
+            <div className="col-span-12 md:col-span-12 rounded-lg overflow-hidden bg-gray-100 h-[320px] p-8 flex gap-12 items-start w-full">
               <div>
                 <div className="flex items-center">
                   <Link href="/">
@@ -111,13 +39,14 @@ export default function Page() {
                     <span className="opacity-70">20 Mushrooms</span>
                   </div>
                 </div>
-                <p className="text-muted-foreground text-sm leading-relaxed pt-4">
+                <p className="text-muted-foreground text-base leading-relaxed pt-6">
                   Welcome to 20 Mushrooms, your new go-to destination for
                   discovering the best dishes and restaurants around you!
-                  Powered by advanced AI, our recommendation engine curates
-                  personalized dining experiences tailored to your taste.
+                  Powered by advanced AI & a community of food enthusiasts, our
+                  recommendation engine curates personalized dining experiences
+                  tailored to your taste.
                 </p>
-                <p className="text-muted-foreground text-sm leading-relaxed pt-4">
+                <p className="text-muted-foreground text-base leading-relaxed pt-4">
                   Whether you&#39;re looking for a hidden gem or sharing your
                   favorite finds, 20 Mushrooms makes it easy to explore and
                   recommend top dishes to fellow food enthusiasts. Join our
@@ -175,10 +104,149 @@ export default function Page() {
         <div className="grid grid-cols-12">
           <div className="col-span-12 md:col-span-12">
             <div className="bg-gray-50 relative z-10 p-8">
-              <h2>How It Works</h2>
-              <Link href="/foobar/explore/boston/restaurant/burger/4">
-                Boston
-              </Link>
+              <h2 className="pb-4">How It Works</h2>
+              <div className="flex items-center gap-8">
+                <div className="min-w-[200px] w-[200px]">
+                  <script
+                    src="https://fast.wistia.com/embed/medias/2h3f4pt7xf.jsonp"
+                    async
+                  ></script>
+                  <script
+                    src="https://fast.wistia.com/assets/external/E-v1.js"
+                    async
+                  ></script>
+                  <div
+                    className="wistia_responsive_padding"
+                    style={{
+                      padding: "100.0% 0 0 0",
+                      position: "relative",
+                    }}
+
+                    // style="padding:100.0% 0 0 0;position:relative;"
+                  >
+                    <div
+                      className="wistia_responsive_wrapper"
+                      style={{
+                        height: "100%",
+                        left: 0,
+                        top: 0,
+                        width: "100%",
+                        position: "absolute",
+                      }}
+                      // style="height:100%;left:0;position:absolute;top:0;width:100%;"
+                    >
+                      <div
+                        className="wistia_embed wistia_async_2h3f4pt7xf seo=false videoFoam=true"
+                        style={{
+                          height: "100%",
+                          width: "100%",
+                          position: "relative",
+                        }}
+                        // style="height:100%;position:relative;width:100%"
+                      >
+                        <div
+                          className="wistia_swatch"
+                          style={{
+                            height: "100%",
+                            left: 0,
+                            top: 0,
+                            opacity: 0.5,
+                            width: "100%",
+                            position: "absolute",
+                            overflow: "hidden",
+                          }}
+                          // style="height:100%;left:0;opacity:0;overflow:hidden;position:absolute;top:0;transition:opacity 200ms;width:100%;"
+                        >
+                          <img
+                            src="https://fast.wistia.com/embed/medias/2h3f4pt7xf/swatch"
+                            style={{
+                              filter: "blur(5px)",
+                              height: "100%",
+                              objectFit: "contain",
+                              width: "100%",
+                            }}
+                            // style="filter:blur(5px);height:100%;object-fit:contain;width:100%;"
+                            alt=""
+                            aria-hidden="true"
+                            // onLoad="this.parentNode.style.opacity=1;"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <p className="text-xs text-muted-foreground pt-4">
+                    &quot;Carmen: Intro&quot; (9s)
+                  </p>
+                </div>
+                <div className="min-w-[200px] w-[200px]">
+                  <script
+                    src="https://fast.wistia.com/embed/medias/3ampi72fyy.jsonp"
+                    async
+                  ></script>
+                  <div
+                    className="wistia_responsive_padding"
+                    style={{
+                      padding: "100.0% 0 0 0",
+                      position: "relative",
+                    }}
+
+                    // style="padding:100.0% 0 0 0;position:relative;"
+                  >
+                    <div
+                      className="wistia_responsive_wrapper"
+                      style={{
+                        height: "100%",
+                        left: 0,
+                        top: 0,
+                        width: "100%",
+                        position: "absolute",
+                      }}
+                      // style="height:100%;left:0;position:absolute;top:0;width:100%;"
+                    >
+                      <div
+                        className="wistia_embed wistia_async_3ampi72fyy seo=false videoFoam=true"
+                        style={{
+                          height: "100%",
+                          width: "100%",
+                          position: "relative",
+                        }}
+                        // style="height:100%;position:relative;width:100%"
+                      >
+                        <div
+                          className="wistia_swatch"
+                          style={{
+                            height: "100%",
+                            left: 0,
+                            top: 0,
+                            opacity: 0.5,
+                            width: "100%",
+                            position: "absolute",
+                            overflow: "hidden",
+                          }}
+                          // style="height:100%;left:0;opacity:0;overflow:hidden;position:absolute;top:0;transition:opacity 200ms;width:100%;"
+                        >
+                          <img
+                            src="https://fast.wistia.com/embed/medias/3ampi72fyy/swatch"
+                            style={{
+                              filter: "blur(5px)",
+                              height: "100%",
+                              objectFit: "contain",
+                              width: "100%",
+                            }}
+                            // style="filter:blur(5px);height:100%;object-fit:contain;width:100%;"
+                            alt=""
+                            aria-hidden="true"
+                            // onLoad="this.parentNode.style.opacity=1;"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>{" "}
+                  <p className="text-xs text-muted-foreground pt-4">
+                    &quot;Carmen: 20 Mushrooms&quot; (10s)
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
